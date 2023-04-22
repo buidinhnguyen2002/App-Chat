7fd31ac4c59076aff53572be84ca8600b89df919
NguyenXuanThanhTrung <20130452@st.hcmuaf.edu.vn>
2023-04-22 20:28:39 +0700

    component archive

---
+++ src/components/archive-item/archive_item.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./archive.scss";
+
+class NavigationItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: props.active,
+            icon: props.icon,
+        }
+    }
+
+    render() {
+        return (
+        <div className="archive">
+            <i className="fa-solid fa-box-archive icon-a"></i>
+            <p className="a-name">Archived</p>
+        </div>
+        )
+    }
+}
+
+
+export default NavigationItem;
--- src/page-t/TestComponent/components-Trung.js
+++ src/page-t/TestComponent/components-Trung.js
@@ -3,6 +3,7 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import Toggle_item from "../../components/toggle_item/toggle_item";
 import Header from "../../components/header/header-chat"
+import Archive_item from "../../components/archive-item/archive_item";
 
 function Components_Trung() {
     return (
@@ -10,6 +11,9 @@ function Components_Trung() {
             <Toggle_item/>
             <div className="header-chat">
                 <Header/>
+                <div className="archive">
+                    <Archive_item/>
+                </div>
             </div>
         </div>
     );
