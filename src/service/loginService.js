24de6696b48bc7a4d77e8c45b2d316919c71d84f
Bui Dinh Nguyen <bdnguyen2k2@gmail.com>
2023-04-29 14:42:36 +0700

    call api register, login

--- src/service/loginService.js
+++ src/service/loginService.js
@@ -1,10 +1,37 @@
 import {w3cwebsocket as W3CWebSocket} from "websocket";
 const client = new W3CWebSocket('ws://140.238.54.136:8080/chat/chat');
+client.onopen = ()=>{
+    console.log('Websocket ')
+}
+client.onmessage = (message) => {
+    const dataFromServer = JSON.parse(message.data);
+    console.log('reply' + dataFromServer);
+}
 export const callAPILogin = (userName, password) => {
-    client.onopen = ()=>{
-        console.log('Websocket ')
-    }
+    client.send(JSON.stringify(
+        {
+            "action": "onchat",
+            "data": {
+                "event": "LOGIN",
+                "data": {
+                    "user": userName,
+                    "pass": password,
+                }
+            }
+        }
+    ));
 }
 export const callAPIRegister = (userName, password) => {
-
+    client.send(JSON.stringify(
+        {
+            "action": "onchat",
+            "data": {
+                "event": "REGISTER",
+                "data": {
+                    "user": userName,
+                    "pass": password,
+                }
+            }
+        }
+    ));
 }
