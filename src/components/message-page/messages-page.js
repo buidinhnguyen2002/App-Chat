df8c779ab9b44271eb0bc5c951aca80418a50bfd
NguyenXuanThanhTrung <20130452@st.hcmuaf.edu.vn>
2023-05-01 13:23:53 +0700

    fix component

--- src/components/message-page/messages-page.js
+++ src/components/message-page/messages-page.js
@@ -9,26 +9,6 @@ class MessagesPageItem extends React.Component {
             icon: props.icon,
         }
     }
-
-    // render() {
-    //     return (
-    //     //     <div className="msg-bottom">
-    //     //         <div className="input-group">
-    //     //             <i className="fa-solid fa-link"></i>
-    //     //             <input type="text" className="form-control"  placeholder="        Write a message..."></input>
-    //     //             <i className="fa-regular fa-face-smile"></i>
-    //     //             <div className="input-group-append ">
-    //     //                     <button className="fa-regular fa-paper-plane"><i className="bi bi-send "></i>
-    //     //                     </button>
-    //     //             </div>
-    //     //         </div>
-    //     //     </div>
-    //     <div className="rectangle-16">
-    //         <div className="message">Write a message ...</div>
-    //     </div>
-    //     <div className="rectangle-17"></div>
-    //     )
-    // }
     render() {
         return (
             <div className="container">
