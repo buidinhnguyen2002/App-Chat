6436a783ebd62b21d21be87fb83596f7816db034
Bui Dinh Nguyen <bdnguyen2k2@gmail.com>
2023-05-03 22:21:56 +0700

    coding chat_detail-header

--- src/page/TestComponent/components_Nguyen.js
+++ src/page/TestComponent/components_Nguyen.js
@@ -4,10 +4,11 @@ import 'mdbreact/dist/css/mdb.css';
 import NavigationItem from "../../components/navigation_item/navigation_item";
 import Chat_item from "../../components/chat_item/chat_item";
 import SearchBar from "../../components/search_bar/search_bar";
+import Chat_detail_header from "../../components/chat_detail_header/chat_detail_header";
 
 function Components_Nguyen() {
     return (
-        <div className="d-flex" style={{backgroundColor: '#F0F4FA', minHeight: '100vh'}}>
+        <div className="d-flex" style={{backgroundColor: '#F0F4FA', minHeight: '100vh', flexWrap: 'wrap'}}>
             <NavigationItem active={1} icon={'bi bi-chat-dots'}/>
             <NavigationItem active={0} icon={'bi bi-chat-dots'}/>
             <NavigationItem active={1} icon={'bi bi-people'}/>
@@ -19,6 +20,7 @@ function Components_Nguyen() {
             <Chat_item />
             <Chat_item isChoose={true}/>
             <SearchBar/>
+            <Chat_detail_header/>
         </div>
     );
 }
