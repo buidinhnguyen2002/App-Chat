3c88f1adc20524f74223abd9e0271b9698ba1001
Bui Dinh Nguyen <bdnguyen2k2@gmail.com>
2023-05-04 18:58:38 +0700

    add change index navigation bar

--- src/components/navigation_bar/navigation_bar.js
+++ src/components/navigation_bar/navigation_bar.js
@@ -4,13 +4,22 @@ import NavigationItem from "../navigation_item/navigation_item";
 import ToggleItem from "../toggle_item/toggle_item";
 import MyAvatar from "../../Assets/Image/my avatar.png";
 import Budgie from "../../Assets/Image/Budgie.png";
+import login from "../../page/login/login";
 
 class NavigationBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            active: props.active,
+            indexActive: 0,
         }
+    }
+    changeIndexNav = (index) => {
+        console.log(index)
+        this.setState(
+            {
+                indexActive: index,
+            }
+        )
     }
     render() {
         return (
@@ -19,17 +28,17 @@ class NavigationBar extends React.Component {
                     <div className="img-top">
                         <img src={Budgie} alt=""/>
                     </div>
-                    <div className="navigation-bar-item">
-                        <NavigationItem active={1} icon={'bi bi-chat-dots'}/>
+                    <div className="navigation-bar-item" onClick={()=>this.changeIndexNav(0)}>
+                        <NavigationItem active={this.state.indexActive === 0} icon={'bi bi-chat-dots'}/>
                     </div>
-                    <div className="navigation-bar-item">
-                        <NavigationItem active={0} icon={'bi bi-people'}/>
+                    <div className="navigation-bar-item" onClick={()=>this.changeIndexNav(1)}>
+                        <NavigationItem active={this.state.indexActive === 1} icon={'bi bi-people'}/>
                     </div>
-                    <div className="navigation-bar-item navigation-bar-item-phone">
-                        <NavigationItem active={0} icon={'bi bi-telephone'}/>
+                    <div className="navigation-bar-item navigation-bar-item-phone" onClick={()=>this.changeIndexNav(2)}>
+                        <NavigationItem active={this.state.indexActive === 2} icon={'bi bi-telephone'}/>
                     </div>
-                    <div className="navigation-bar-item navigation-bar-item-setting">
-                        <NavigationItem active={0} icon={'bi bi-gear'}/>
+                    <div className="navigation-bar-item navigation-bar-item-setting" onClick={()=>this.changeIndexNav(3)}>
+                        <NavigationItem active={this.state.indexActive === 3} icon={'bi bi-gear'}/>
                         <div className="horizontal-line"></div>
                     </div>
                 </div>
